21a005fc1b2d1424da5d0be3b238a148832e53c9
FlutterRaj <rajkar921@gmail.com>
2020-09-27 12:38:04 +0530

    advance pattern added

--- Pattern exercise/advance_pattern.py
+++ Pattern exercise/advance_pattern.py
@@ -1,4 +1,4 @@
-# pattern --> 1
+# # pattern --> 1
 
 for i in range(1,10,2):
 	for j in range(i,10,2):
@@ -7,40 +7,40 @@ for i in range(1,10,2):
 		print(k,end=" ")
 	print()
 
-# output -->
-# 1 3 5 7 9 
-# 3 5 7 9 1 
-# 5 7 9 1 3 
-# 7 9 1 3 5 
-# 9 1 3 5 7 
+# # output -->
+# # 1 3 5 7 9
+# # 3 5 7 9 1
+# # 5 7 9 1 3
+# # 7 9 1 3 5
+# # 9 1 3 5 7
 
-# pattern --> 2
+# # pattern --> 2
 
 for i in range(5,0,-2):
 	for j in range(i,0,-1):
 		print(i,end=" ")
 	print()
 
-# output -->
-# 5 5 5 5 5 
-# 3 3 3 
-# 1 
+# # output -->
+# # 5 5 5 5 5
+# # 3 3 3
+# # 1
 
-# pattern --> 3
+# # pattern --> 3
 
 for i in range(1,10,2):
 	for j in range(i,0,-2):
 		print(i,end=" ")
 	print()
 
-# output -->
-# 1 
-# 3 3 
-# 5 5 5 
-# 7 7 7 7 
-# 9 9 9 9 9 
+# # output -->
+# # 1
+# # 3 3
+# # 5 5 5
+# # 7 7 7 7
+# # 9 9 9 9 9
 
-# pattern --> 4
+# # pattern --> 4
 
 k = 15
 for i in range(5,0,-1):
@@ -49,9 +49,78 @@ for i in range(5,0,-1):
 		k -= 1;
 	print()
 
+
 # output -->
-# 15 14 13 12 11 
-# 10 9 8 7 
-# 6 5 4 
-# 3 2 
+# 15 14 13 12 11
+# 10 9 8 7
+# 6 5 4
+# 3 2
 # 1
+
+
+# pattern --> 5
+
+count = 1
+for i in range(1,5):
+	for j in range(i):
+		print(count, end=" ")
+		count += 1
+	print()
+
+# output -->
+# 1
+# 2 3
+# 4 5 6
+# 7 8 9 10
+
+
+# pattern --> 6
+
+for i in range(4):
+    for j in range(4):
+        if(j == 3 or i == 3 or i == 0 or j == 0):
+            print("@", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
+
+
+# output -->
+# @ @ @ @
+# @     @
+# @     @
+# @ @ @ @
+
+
+# pattern --> 7
+
+for i in range(1,6):
+	for j in range(i):
+		print(i,end=" ")
+	print()
+
+
+# output -->
+# 1
+# 2 2
+# 3 3 3
+# 4 4 4 4
+# 5 5 5 5 5
+
+# pattern --> 8
+
+n = 11
+for i in range(1, 6):
+    for j in range(1, n-i, 2): 
+        print(j, end=" ")
+    n -= 1	   
+    print()					 
+
+# output -->
+# 1 3 5 7 9 
+# 1 3 5 7 
+# 1 3 5 
+# 1 3 
+# 1 
+
+# ---------------  © All copyrights reserved by Raj -------------------- #
---
+++ Pattern exercise/pascal triangle.py
@@ -0,0 +1,33 @@
+#           1   
+#         1   1   
+#       1   2   1   
+#     1   3   3   1   
+#   1   4   6   4   1   
+# 1   5   10  10  5   1 
+
+# 1
+# 1 1
+# 1 2 1
+# 1 3 3  1
+# 1 4 6  4  1
+# 1 5 10 10 5  1
+
+n = 6
+list1 = []
+
+for i in range(n):
+    temp_list = []
+    for j in range(i+1):
+        if (j == 0 or j == i):
+            temp_list.append(1)
+        else:
+            temp_list.append(list1[i - 1][j - 1]
+							 + list1[i - 1][j])
+    list1.append(temp_list)
+
+for i in range(n):
+	for j in range(n-i-1):
+		print(format(" ","<2"),end="")
+	for j in range(i+1):
+		print(format(list1[i][j],"<3"),end=" ")
+	print()
