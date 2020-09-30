346e1e71e16ad9ae0acf866782a83d83b75fd2be
FlutterRaj <rajkar921@gmail.com>
2020-09-30 23:26:56 +0530

    Nptel week 2 and 3 programming solutions

---
+++ Nptel/week- 2 solutions.py
@@ -0,0 +1,20 @@
+# <------ solution programming assignment 1 -------> #
+print('IIT ROPAR',end="")
+
+# <------ solution programming assignment 2 -------> #
+rupee = int(input())
+
+print(rupee*80,end="")
+
+# <------ solution programming assignment 3 -------> #
+cost = int(input())
+sell = int(input())
+
+if cost > sell:
+  print("Loss",end="")
+elif cost < sell:
+  print("Profit",end="")
+else:
+  print("Neither",end="")
+
+# <-------- Practiced purposed only < Raj™ /> --------------> #
---
+++ Nptel/week- 3 solutions.py
@@ -0,0 +1,38 @@
+# <------ solution programming assignment 1 -------> #
+s1 = int(input())
+s2 = int(input())
+s3 = int(input())
+s4 = int(input())
+s5 = int(input())
+
+print((s1 + s2 + s3 + s4 + s5) / 5, end="")
+
+# <------ solution programming assignment 2 -------> #
+list_1 = []
+for i in range(1, 51):
+    list_1.append(i)
+
+a, b = input().split()
+a = int(a)
+b = int(b)
+
+new_list = list_1[a:b]
+for i in new_list:
+    print(i)
+
+# <------ solution programming assignment 3 -------> #
+list_1 = []
+for i in range(1, 51):
+    list_1.append(i)
+
+num = int(input())
+
+count = 0
+list_1 = list_1[num:]
+for i in list_1:
+    if i % num == 0:
+        count += 1
+
+print(count, end="")
+
+# <-------- Practiced purposed only < Raj™ /> --------------> #
