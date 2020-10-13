e5e2bf5bc671e1dbaf3355b192397db668795eb6
FlutterRaj <rajkar921@gmail.com>
2020-10-13 23:05:06 +0530

    robot problem solution added

--- Nptel/week- 5 solutions.py
+++ Nptel/week- 5 solutions.py
@@ -19,9 +19,26 @@ print(square_dict, end="")
 
 '''
 Question 2 --> Robot problem 
+Original Credit - @Stack-Over-flow
 '''
 
-# On Under Process --> I Try to understand what is a 2D plane :(
+pos = {"x": 0, "y": 0}
+num = int(input())
+
+for _ in range(num):
+    command = input().split(" ")      # ACCEPT MOVEMENT COMMAND AND STORE AS A LIST
+    if command[0] == "UP":             # EXTRACT DIRECTION AND COMPARE
+        # INCREMENT/DECREMENT APPROPRIATE CO-ORDINATES
+        pos["y"] += int(command[1])
+    if command[0] == "DOWN":
+        pos["y"] -= int(command[1])
+    if command[0] == "LEFT":
+        pos["x"] -= int(command[1])
+    if command[0] == "RIGHT":
+        pos["x"] += int(command[1])
+
+print(int(round((pos["x"]**2 + pos["y"]**2)**0.5)),
+      end="")   # DISTANCE FROM ORIGIN
 
 
 '''
