d6233c8ef8447b3a288196099a65008ab5d22492
FlutterRaj <rajkar921@gmail.com>
2020-10-13 22:32:50 +0530

    nptel week 5 program solution

---
+++ Nptel/week- 5 solutions.py
@@ -0,0 +1,43 @@
+'''
+Week - 5 --> Programming solutions
+Question 1 --> Dict Sqaure
+'''
+# using basic method
+n = int(input())
+square = {}
+
+for i in range(1, n+1):
+    square[i] = i * i
+
+
+print(square, end="")
+
+# using Dictionary Comprehension
+n = int(input())
+square_dict = {num: num*num for num in range(1, n+1)}
+print(square_dict, end="")
+
+'''
+Question 2 --> Robot problem 
+'''
+
+# On Under Process --> I'm Try to understand what is a 2D plane :(
+
+
+'''
+Question 3 --> Dict Sqaure [same as previous]
+'''
+# using basic method
+n = int(input())
+square = {}
+
+for i in range(1, n+1):
+    square[i] = i * i
+
+
+print(square, end="")
+
+# using Dictionary Comprehension
+n = int(input())
+square_dict = {num: num*num for num in range(1, n+1)}
+print(square_dict, end="")
